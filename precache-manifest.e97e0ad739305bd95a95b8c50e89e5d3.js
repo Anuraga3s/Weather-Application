2b1d61d5948621d4318131c3243d24471687bf6a
Anurag-cse <anurag14082003@gmail.com>
2025-09-22 23:21:12 +0530

    Updates

---
+++ precache-manifest.e97e0ad739305bd95a95b8c50e89e5d3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d05b50496c159aa33f6b19aa10073fe",
+    "url": "/Anuraga3s/Weather-Application/index.html"
+  },
+  {
+    "revision": "55873f9b0fafdac670f2",
+    "url": "/Anuraga3s/Weather-Application/static/css/main.56af8d5f.chunk.css"
+  },
+  {
+    "revision": "001b67d05a335e326bff",
+    "url": "/Anuraga3s/Weather-Application/static/js/2.6a7fe4e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Anuraga3s/Weather-Application/static/js/2.6a7fe4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55873f9b0fafdac670f2",
+    "url": "/Anuraga3s/Weather-Application/static/js/main.b4e6ced3.chunk.js"
+  },
+  {
+    "revision": "a7ea03a5d74d2f0b1757",
+    "url": "/Anuraga3s/Weather-Application/static/js/runtime-main.98b1d892.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/Anuraga3s/Weather-Application/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "75e0f8ca422777deab777d3abe0a90e7",
+    "url": "/Anuraga3s/Weather-Application/static/media/background.75e0f8ca.jpg"
+  },
+  {
+    "revision": "e1698332435d6a6fb48294f85157aeab",
+    "url": "/Anuraga3s/Weather-Application/static/media/city.e1698332.jpg"
+  }
+]);
