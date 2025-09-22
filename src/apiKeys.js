d1386a8e0c7d7cdd5f32027b6b87fcb7f7fd56ee
Anurag-cse <anurag14082003@gmail.com>
2025-09-22 23:07:09 +0530

    Initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "f6849d30e9653f372e741612085893ab",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
